e1f880e1efd4d11dc8ab33ccfedbc19b63e2eafa
Mina Malak <mina.malak@tactful.ai>
2022-04-07 00:06:29 +0200

    finished navBar and started in second section

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import "./App.css";
-import { Alert } from "react-bootstrap";
+import { Navbar, Nav, Badge,Button } from "react-bootstrap";
+import img from "./assets/img.svg";
+import cart from "./assets/cart.svg";
+import data from "./data/db.json";
 
 function App() {
-  let x: Number = 6;
+  // const products =JSON.parse(data).products as [];
+  console.log(data)
+  // const featuredProduct = JSON.parse(data).filter(product => product.featured === true);
   return (
-    <div className="">
-      {x}
-      <br />
-      {[
-        "primary",
-        "secondary",
-        "success",
-        "danger",
-        "warning",
-        "info",
-        "light",
-        "dark",
-      ].map((variant, idx) => (
-        <Alert key={idx} variant={variant}>
-          This is a {variant} alert—check it out!
-        </Alert>
-      ))}
+    <div className="container container-fluid py-3">
+      {/* NavBar  */}
+      <Navbar bg="transparent" expand={false}>
+        <div className="d-flex justify-content-space-between">
+          <Navbar.Brand href="#">
+            <img src={img} width="220" height="auto" />
+          </Navbar.Brand>
+          <Nav className="nav-item">
+            <img src={cart} width="54" height="auto" alt="cart" />
+            <Badge bg="black" text="white" className="nav-item__badge">
+              9
+            </Badge>
+          </Nav>
+        </div>
+      </Navbar>
+      <hr />
+      {/* Main section */}
+      <section className="main-section row justify-content-space-between d-flex pos-relative">
+        <div className="col-lg-5">
+          <p className="main-section__header">Recycled Plastic</p>
+        </div>
+        <div className="col-md-3 col-sm-12">
+          <Button variant="dark" className="main-section__button">Add to cart</Button>
+        </div>
+        <div className="col-lg-12 ">
+        </div>
+      </section>
+      
     </div>
   );
 }
