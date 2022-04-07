8dc1931a2d8573b5057217e70653a111f8ca582d
Mina Malak <mina.malak@tactful.ai>
2022-04-07 17:27:15 +0200

    finished featured product

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,10 @@ import { Navbar, Nav, Badge,Button } from "react-bootstrap";
 import img from "./assets/img.svg";
 import cart from "./assets/cart.svg";
 import data from "./data/db.json";
-
+import {IProduct} from "./Interface/IProduct";
 function App() {
-  // const products =JSON.parse(data).products as [];
-  console.log(data)
-  // const featuredProduct = JSON.parse(data).filter(product => product.featured === true);
+  const products =data.products;
+  const featuredProduct =products.filter(product => product.featured === true)[0] as IProduct;
   return (
     <div className="container container-fluid py-3">
       {/* NavBar  */}
@@ -28,12 +27,14 @@ function App() {
       {/* Main section */}
       <section className="main-section row justify-content-space-between d-flex pos-relative">
         <div className="col-lg-5">
-          <p className="main-section__header">Recycled Plastic</p>
+          <p className="main-section__header">{featuredProduct.name}</p>
         </div>
         <div className="col-md-3 col-sm-12">
           <Button variant="dark" className="main-section__button">Add to cart</Button>
         </div>
-        <div className="col-lg-12 ">
+        <div className="col-lg-12 main-section__img-container p-0">
+          <span className="main-section__img-container__description">Featured</span>
+          <img src={featuredProduct.image.src} alt="product" width="100%"/>
         </div>
       </section>
       
---
+++ src/Interface/IProduct.ts
@@ -0,0 +1,19 @@
+export interface IImage{
+    src:string;
+    alt:string;
+}
+export interface IProduct{
+    name: string;
+    category: string;
+    price: number;
+    currency: string;
+    image:IImage;
+    bestseller:boolean;
+    featured:boolean;
+    details?:{
+        description:string;
+        weight:number;
+        thickness:number;
+        recommedations?:IImage[];
+    }
+}
