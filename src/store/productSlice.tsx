8d4b7a919240ee5fad5db661da7f59a277ac5753
Mina Malak <mina.malak@tactful.ai>
2022-04-09 23:33:29 +0200

    solve errors of redux-toolkit

--- src/store/productSlice.tsx
+++ src/store/productSlice.tsx
@@ -8,21 +8,29 @@ const productSlice = createSlice({
     name: "product",
     initialState: initalState,
     reducers: {
-      getProductByFilter: (state, action) => {
+      getProductByFilter: (state:any, action) => {
         const { category, priceRange } = action.payload;
-        let products = {...state.products};
-        products = products.filter((product) => {
+        let currentProducts = {...state};
+        currentProducts = currentProducts.filter((product) => {
           if (category === product.category && (priceRange.min <= product.price &&
             product.price <= priceRange.max)) {
                  return product;
           }
         });
-        state.products = products;
-      }
+        state.products = currentProducts;
+      },
+      sortProducts: (state:any, { payload: { sortName } }) => {
+        const productsCopy = {...state};
+        if (sortName === "price") {
+          state.products = productsCopy.sort((a:IProduct, b:IProduct) => a.price - b.price);
+        } else {
+          state.products = productsCopy.sort((a:IProduct, b:IProduct) => a.name.localeCompare(b.name));
+        }
+      },
     },
   });
   
-// export const { getProductByFilter, sortProducts } = productSlice.actions;
+export const { getProductByFilter, sortProducts } = productSlice.actions;
 export const selectProduct = (state:{product:IProduct}) => state.product;
 export default productSlice.reducer;
   
