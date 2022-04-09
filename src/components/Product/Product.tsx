efddc0dd8a177e1746d7a39bc50d493294191e64
Mina Malak <mina.malak@tactful.ai>
2022-04-09 20:50:15 +0200

    add to cart button done

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import "../../App.css";
 import { Card } from "react-bootstrap";
 import { IProduct } from "../../Interface/IProduct";
@@ -9,10 +10,27 @@ function Product(props: IProps) {
   function clickHandler() {
     console.log("clicked");
   }
+  const [buttonShow, setButtonShow] = useState(false);
   return (
-    <div className="product-list__items__products__container__product">
-      <img src={product.image.src} width="100%" height="100%" className="product-list__items__products__container__product__img"/>
-    </div>
+    <>
+      <div
+        className="product-list__items__products__container__product"
+        onMouseEnter={() => setButtonShow(true)}
+        onMouseLeave={() => setButtonShow(false)}
+      >
+        <img
+          src={product.image.src}
+          width="100%"
+          height="100%"
+          className="product-list__items__products__container__product__img"
+        />
+        {buttonShow && (
+          <button className="btn btn-dark product-list__items__products__container__product__button">
+            Add to cart
+          </button>
+        )}
+      </div>
+    </>
   );
 }
 
