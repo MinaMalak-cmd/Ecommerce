5b5f4e72ecafdd36db0fbe81a6ec39b2d107e9b1
Mina Malak <mina.malak@tactful.ai>
2022-04-10 19:04:26 +0200

    integrate two separate slices

---
+++ src/store/index.tsx
@@ -0,0 +1,54 @@
+import { createSlice, configureStore} from "@reduxjs/toolkit";
+import {IProduct} from "../Interface/IProduct";
+import data from "../data/db.json";
+
+const tempProducts = data.products;
+const productInitialState = tempProducts;
+const productSlice = createSlice({
+    name: "product",
+    initialState: productInitialState,
+    reducers: {
+      getProductByFilter: (state:any, action) => {
+        const { category, priceRange } = action.payload;
+        let currentProducts = tempProducts.filter((product) => {
+          if (category === product.category && (priceRange.min <= product.price &&
+            product.price <= priceRange.max)) {
+                 return product;
+          }
+        });
+        state.product = currentProducts;
+      },
+      sortProducts: (state:any, { payload: { sortName } }) => {
+        const productsCopy = {...state};
+        if (sortName === "price") {
+          state.product = productsCopy.sort((a:any, b:any) => a.price - b.price);
+        } else {
+          state.product = productsCopy.sort((a:any, b:any) => a.name-b.name);
+        }
+      },
+    },
+  });
+
+const cartInitialState = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : [];
+const cartSlice = createSlice({
+    name: "cart",
+    initialState: cartInitialState,
+    reducers: {
+      addToCart: (state:any, action) => {
+        let currentProducts = {...cartInitialState};
+        currentProducts.push(state);
+        state.cart = currentProducts;
+        localStorage.setItem('cart', JSON.stringify([state.cart]));
+      },
+      removeFromCart: (state:any, action) => {
+        state.cart = [];
+        localStorage.setItem('cart', JSON.stringify([state.cart]));
+      }
+    },
+  });
+const store = configureStore({
+  reducer: { product: productSlice.reducer, cart: cartSlice.reducer },
+});
+export const productActions = productSlice.actions;
+export const cartActions = cartSlice.actions;
+export default store;
