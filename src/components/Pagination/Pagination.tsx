890605d07cca0ebe45d5af82942a717b5948b13b
Mina Malak <mina.malak@tactful.ai>
2022-05-21 14:24:05 +0200

    add pagination layout without enable first and previous buttons

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { Pagination as BPagination } from "react-bootstrap";
 
 function Pagination() {
+  let active = 2;
+  let items = [];
+  for (let number = 1; number <= 5; number++) {
+    items.push(number);
+  }
+  function choosePageNumber(number: any) {
+   console.log(number);
+  }
   return (
-    <div className="mt-3 text-center col-12">
-      Pagination
+    <div className="mt-3 text-center col-12 m-auto w-25">
+      <BPagination>
+        <BPagination.First />
+        <BPagination.Prev />
+        {items.map((item) => (
+          <BPagination.Item key={item} active={item === active} onClick={()=>choosePageNumber(item)}>
+            {item}
+          </BPagination.Item>
+        ))}
+        <BPagination.Next />
+        <BPagination.Last />
+      </BPagination>
     </div>
   );
 }
