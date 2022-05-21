ea188d0ae794fd86e378f00bbbc434dd51dade9f
Mina Malak <mina.malak@tactful.ai>
2022-05-21 14:01:51 +0200

    init pagination component

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Pagination() {
+  return (
+    <div className="mt-3 text-center col-12">
+      Pagination
+    </div>
+  );
+}
+
+export default Pagination;
